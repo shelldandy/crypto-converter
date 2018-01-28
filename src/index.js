6f16bc1c35f7accd633adc852d4b3494681c6fdb
Miguel Palau <mpalauzarza@gmail.com>
2018-01-28 17:28:50 -0600

    hmr and base video

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(<App />, rootEl);
+
+// Hot module replacement
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    )
+  })
+}
+
 registerServiceWorker();
